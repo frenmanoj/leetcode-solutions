6fee1f3b3d69475af7e348aaa908ef4ca57d5a0a
Ganesh <gtiwari333@gmail.com>
2018-04-03 22:01:04 -0500

    added some logic

--- src/algorithm/dynamicprogramming/Test.java
+++ src/algorithm/dynamicprogramming/Test.java
@@ -2,6 +2,6 @@ package algorithm.dynamicprogramming;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("Namaskar");
     }
 }
